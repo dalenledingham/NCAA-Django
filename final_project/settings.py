8a731a62555bec19102b309f0bfad3f846bcc236
dalen <dalen.ledingham@gmail.com>
2021-07-08 18:36:21 -0500

    Troubleshooting error pages

--- final_project/settings.py
+++ final_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,7 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'users:login'
 
 # Heroku settings
-import os
+
 import django_heroku
 django_heroku.settings(locals())
 
